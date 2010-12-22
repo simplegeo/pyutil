730245a62a105e7ab31a7418928818441898f7c8
Simple Geebus <nerds+geebus@simplegeo.com>
2010-12-22 18:36:18 +0000

    Prepared changelog for build 57

--- pyutil/_version.py
+++ pyutil/_version.py
@@ -5,7 +5,7 @@
 # -NN "build number" suffix, or else a -rNN "revision number" suffix. Please see
 # pyutil.version_class for a description of what the different fields mean.
 
-verstr = "1.8.1.56"
+verstr = "1.8.1.57"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
