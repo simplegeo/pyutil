640e444eddef5a2dca7253ca6b73228fe73ad482
Zooko Ofsimplegeo <zooko@simplegeo.com>
2011-01-07 10:30:48 -0700

    new version

--- pyutil/_version.py
+++ pyutil/_version.py
@@ -6,7 +6,7 @@
 # pyutil.version_class for a description of what the different fields mean.
 
 __pkgname__ = "pyutil"
-verstr = "1.8.1-r245"
+verstr = "1.8.1-r246"
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
